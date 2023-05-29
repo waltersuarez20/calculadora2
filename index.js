19a3b1c717ce96a844e6a620ab533d1a756dd4da
waltersuarez20 <127705995+waltersuarez20@users.noreply.github.com>
2023-05-28 22:24:09 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
-const express=require('express');
+const express = require("express");
+
+
+
 const app= express();
 
 app.use(express.json());
 
-
-app.use( function(req, res, next){
+app.use{function(req, res ,next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Methos", "POST");
     res.header("Access-Control-Allow-Headers", "Content-type");
-    next();
+
 }
-);
 
-
-
-app.post(
-    '/api/sumar',
-     (req, res)=>{
-        console.log("¡Alguien está conectándose a esta ruta!");        
-        const {numero_1, numero_2}= req.body;
-        const resultado=parseFloat(numero_1)+parseFloat(numero_2);        
-        res.json(resultado);
-    }
-);
+}
 
 app.post(
-    '/api/restar',
+    "/sumar",
     (req, res)=>{
-        const {numero_1, numero_2}= req.body;
-        const resultado=numero_1-numero_2;        
-        res.json(resultado);
+        console.log("alguien esta conectandose a esta ruta!!");
+        const (numer0_1, numero_2)-req.body;
+        const resultado=numero_1-numero_2;
+
+
+        res.json(req.body);
     }
+
 );
 
 app.post(
-    '/api/multiplicar',
+    "/restar",
     (req, res)=>{
-        const {numero_1, numero_2}= req.body;
-        const resultado=numero_1*numero_2;        
+        const {numer0_1, numero_2}-req.body;
+        const resultado=numero_1-numero_2;
         res.json(resultado);
     }
-);
+    );
 
-app.post(
-    '/api/dividir',
+app.post (
+    "/api/dividir",
     (req,res)=>{
-             const {numero_1, numero_2}= req.body;           
-            const resultado=numero_1/numero_2;   
-            res.json(resultado);
+        let resultado;
+       try{
+        const{numero_1, numero_2}= req.body;
+        let resultado;
+        resultado=numero_1/numero_2;
+       }catch(error)
+
+       
+    resultado=error;
+
     }
-)
+res.json(resultado);
+);
 
 
-app.listen(
-    3000,
+app.listen( 3000,
     ()=>{
-      console.log("Servidor ejecutándose en el puerto 3000");
+        console.log("servidor ejecuntandose en el puerto 3000");
+
     }
 );
+
+
